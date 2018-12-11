318a7017f9cf03e8edd8d3f8b718496af4565e56
Sergey Vasilenko <vasilenko.sn@gmail.com>
2018-12-11 11:10:19 +0300

    apiserver.py styling

--- backend/apiserver.py
+++ backend/apiserver.py
@@ -10,60 +10,69 @@ from models.eth_model import EthereumClass
 from models.generate import decrypt_seed
 import json
 
-from aiohttp.web_request import BaseRequest
+from aiohttp.web_request import BaseRequest, Request
+
 PORT = 3200
 
 accounting = AccounttingAPI(None)
 
+
 async def check(request):
-    return {"error" : None, "result" : "server"}
+    return {"error": None, "result": "server"}
 
 
-async def login(request : BaseRequest):
+async def login(request: Request):
     res = accounting.login(request.all_data["user"], request.all_data["password"])
     if res is None:
-        return {"error" : "Problem with login", "result" : None}
-    return {"error" : None, "result" : res}
+        return {"error": "Problem with login", "result": None}
+    return {"error": None, "result": res}
 
-async def withdrawal_requests(request : BaseRequest):
+
+async def withdrawal_requests(request: Request):
     res = accounting.get_reported_transactions()
-    return {"error" : None, "result" : res}
+    return {"error": None, "result": res}
 
-async def get_mnemonic(request : BaseRequest):
+
+async def get_mnemonic(request: Request):
     res = generate_mnemonic()
     return {"error": None, "result": res}
 
-async def add_wallet(request : BaseRequest):
+
+async def add_wallet(request: Request):
     wallet_id = request.match_info.get('wallet', None)
     config = load_config()
     if wallet_id in config and "encrypted_seed" in config["wallet_id"]:
-        return {"error" : "Seed phrase is already set"}
-    encrypted_seed, btc_pub, eth_pub = generate_encrypted_seed(request.all_data["mnemonic"], request.all_data["keypassword"])
-    cfg = {wallet_id : {"type": "wallet", "wallettype": request.all_data["wallettype"], "encrypted_seed" : str(encrypted_seed), "BTC" : btc_pub, "ETH" : eth_pub }}
+        return {"error": "Seed phrase is already set"}
+    encrypted_seed, btc_pub, eth_pub = generate_encrypted_seed(request.all_data["mnemonic"],
+                                                               request.all_data["keypassword"])
+    cfg = {wallet_id: {"type": "wallet", "wallettype": request.all_data["wallettype"],
+                       "encrypted_seed": str(encrypted_seed), "BTC": btc_pub, "ETH": eth_pub}}
     save_config(cfg)
-    return {"error" : None, "result" : True}
+    return {"error": None, "result": True}
 
-async def get_wallets(request : BaseRequest):
+
+async def get_wallets(request: Request):
     config = load_config()
-    wallets=[]
+    wallets = []
     for w in config:
         if config[w].get("type", None) == "wallet":
             wallets.append(w)
 
-    return {"error" : None, "result" : wallets}
+    return {"error": None, "result": wallets}
 
-async def get_wallet(request : BaseRequest):
+
+async def get_wallet(request: Request):
     config = load_config()
     outs = load_outs_file()
     wallet_id = request.match_info.get('wallet', None)
-    res={
-        "BTC": {"pub": config[wallet_id]["BTC"], "outs" : outs.get(wallet_id,{}).get("BTC",{}).get("outs", None)},
-        "ETH": {"pub": config[wallet_id]["ETH"], "outs" : outs.get(wallet_id,{}).get("ETH",{}).get("outs", None)}
-        }
-    return {"error" : None, "result" : res}
+    res = {
+        "BTC": {"pub": config[wallet_id]["BTC"], "outs": outs.get(wallet_id, {}).get("BTC", {}).get("outs", None)},
+        "ETH": {"pub": config[wallet_id]["ETH"], "outs": outs.get(wallet_id, {}).get("ETH", {}).get("outs", None)}
+    }
+    return {"error": None, "result": res}
 
 
-async def get_balance(request : BaseRequest):
+async def get_balance(request: Request):
     wallet_id = request.match_info.get('wallet', None)
     currency = request.match_info.get('currency', None)
     number = request.match_info.get('number', None)
@@ -82,10 +91,10 @@ async def get_balance(request : BaseRequest):
             return {"error": "Cannot get address"}
         addr = ethereum.get_addr_from_pub(pubkey, number)
         balance = ethereum.get_balance(addr)
-    return {"error" : None, "result" : balance }
+    return {"error": None, "result": balance}
 
 
-async def get_address(request : BaseRequest):
+async def get_address(request: BaseRequest):
     wallet_id = request.match_info.get('wallet', None)
     currency = request.match_info.get('currency', None)
     number = request.match_info.get('number', None)
@@ -101,9 +110,10 @@ async def get_address(request : BaseRequest):
             return {"error": "Cannot get address"}
         addr = EthereumClass().get_addr_from_pub(pubkey, number)
 
-    return {"error" : None,  "result" : addr}
+    return {"error": None, "result": addr}
 
-async def set_outs(request : BaseRequest):
+
+async def set_outs(request: BaseRequest):
     wallet_id = request.match_info.get('wallet', None)
     currency = request.match_info.get('currency', None)
     password = request.all_data.get("password")
@@ -121,20 +131,21 @@ async def set_outs(request : BaseRequest):
     verify_result = btc.verify_data(json.dumps(outs), signature, _pub)
     if verify_result:
         save_outs_to_file(wallet_id, currency, outs, signature)
-    return {"error" : None,  "result" : verify_result}
+    return {"error": None, "result": verify_result}
+
 
 routes = [
-    ("*",       r"/api/check/", check),
-    ("*",       r"/api/", check),
-    ("GET",  r"/api/mnemonic/", get_mnemonic),
+    ("*", r"/api/check/", check),
+    ("*", r"/api/", check),
+    ("GET", r"/api/mnemonic/", get_mnemonic),
     ("POST", r"/api/wallets/{wallet}/", add_wallet),
-    ("GET",  r"/api/wallets/", get_wallets),
-    ("GET",  r"/api/wallets/{wallet}/", get_wallet),
-    ("GET",  r"/api/wallets/{wallet}/{currency}/{number}/address/", get_address),
-    ("GET" , r"/api/wallets/{wallet}/{currency}/{number}/balance/", get_balance),
-    ("POST" , r"/api/wallets/{wallet}/{currency}/outs/", set_outs),
+    ("GET", r"/api/wallets/", get_wallets),
+    ("GET", r"/api/wallets/{wallet}/", get_wallet),
+    ("GET", r"/api/wallets/{wallet}/{currency}/{number}/address/", get_address),
+    ("GET", r"/api/wallets/{wallet}/{currency}/{number}/balance/", get_balance),
+    ("POST", r"/api/wallets/{wallet}/{currency}/outs/", set_outs),
     ("POST", r"/api/login/", login),
-    ("GET",  r"/api/tokens/requests", withdrawal_requests),
+    ("GET", r"/api/tokens/requests", withdrawal_requests),
 ]
 
 
@@ -142,7 +153,7 @@ async def init(loop, host="0.0.0.0", port=PORT):
     app = web.Application(
         loop=loop,
         client_max_size=128 * 1024 * 1024,
-        middlewares=[cors_middleware,error_handling_middleware, request_and_auth_data_middleware]
+        middlewares=[cors_middleware, error_handling_middleware, request_and_auth_data_middleware]
     )
 
     for route in routes:
@@ -152,7 +163,6 @@ async def init(loop, host="0.0.0.0", port=PORT):
     app.router.add_static('/', path='../frontend/', name='static')
 
     loop.set_default_executor(ThreadPoolExecutor(max_workers=3))
-
 
     handler = app.make_handler(access_log_format="%s %r (%a) %Tfsec")
     server_generator = loop.create_server(handler, host, port)
@@ -167,16 +177,21 @@ async def shutdown(server, app, handler):
     # await handler.finish_connections(10.0)
     await app.cleanup()
 
-remote_loop = asyncio.get_event_loop()
-server_generator, handler, app = remote_loop.run_until_complete(init(remote_loop, port=PORT))
-server = remote_loop.run_until_complete(server_generator)
 
-try:
-    print("Starting IOLoop...")
-    remote_loop.run_forever()
-except KeyboardInterrupt:
-    print("Starting shutdown...")
-finally:
-    remote_loop.run_until_complete(shutdown(server, app, handler))
-    remote_loop.close()
+def main():
+    remote_loop = asyncio.get_event_loop()
+    server_generator, handler, app = remote_loop.run_until_complete(init(remote_loop, port=PORT))
+    server = remote_loop.run_until_complete(server_generator)
 
+    try:
+        print("Starting IOLoop...")
+        remote_loop.run_forever()
+    except KeyboardInterrupt:
+        print("Starting shutdown...")
+    finally:
+        remote_loop.run_until_complete(shutdown(server, app, handler))
+        remote_loop.close()
+
+
+if __name__ == "__main__":
+    main()
